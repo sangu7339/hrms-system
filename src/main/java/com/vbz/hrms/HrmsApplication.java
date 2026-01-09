f438e9c1e31bb9949697ef73524ffbe87d1683cb
sangu7339 <seegisangmesh@gmail.com>
2026-01-09 10:54:09 +0530

    aaa

--- src/main/java/com/vbz/hrms/HrmsApplication.java
+++ src/main/java/com/vbz/hrms/HrmsApplication.java
@@ -8,8 +8,7 @@ public class HrmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 		System.out.println("Running successfully");
-		System.out.println("                     assamualaikum lyari");
-		
+	
 			  
 		
 	}
